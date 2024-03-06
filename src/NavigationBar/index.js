e945d5f236e86b6856150fca654077c6b6e7976e
fong-amanda <fong.m.amanda@gmail.com>
2024-03-06 15:42:02 -0500

    nav bar collapse

--- src/NavigationBar/index.js
+++ src/NavigationBar/index.js
@@ -1,11 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from "react";
+import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
 import homeImage from "../Images/AFongLogo.png";
 import { scroller } from 'react-scroll';
 import "./nav.css";
 
-function Nav() {
+function NavigationBar() {
   const [activeLink, setActiveLink] = useState("Home");
 
   const handleScrollToPortfolio = () => {
@@ -36,35 +36,50 @@ function Nav() {
     } else {
       setActiveLink(null);
     }
-  }, [activeLink]);
+  }, []);
 
   return (
-    <nav className="logo nav">
-      <div className="logo">
-        <NavLink to="/Home" className={` ${activeLink === "Home" ? "active" : ""}`}>
-          <img src={homeImage} alt="Amanda Fong's Logo" width="120px" height="120px" />
-        </NavLink>
-      </div>
-      <div className="header-right">
-        <NavLink
-          to="/Home#my-work"
-          className={` ${activeLink === "Home" ? "active" : ""}`}
-          onClick={handleScrollToPortfolio}
-        >
-          My&nbsp;Work
-        </NavLink>
-        <NavLink to="/Scrapbook" className={` ${activeLink === "Scrapbook" ? "active" : ""}`}>
-          Scrapbooking
-        </NavLink>
-        <NavLink to="/Play" className={`${activeLink === "Play" ? "active" : ""}`}>
-          Play
-        </NavLink>
-        <NavLink to="/About" className={` ${activeLink === "About" ? "active" : ""}`}>
-          About
-        </NavLink>
-      </div>
-    </nav>
+    <Navbar expand="md" bg="" variant="">
+      <Navbar.Brand as={NavLink} to="/Home" className={activeLink === "Home" ? "active" : ""}>
+        <img src={homeImage} alt="Amanda Fong's Logo" width="120px" height="120px" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav " />
+
+      <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+        <Nav className="ml-auto ">
+          <NavLink
+            as={NavLink}
+            to="/Home#my-work"
+            className={activeLink === "Home" ? "active" : ""}
+            onClick={handleScrollToPortfolio}
+          >
+            My Work
+          </NavLink>
+          <NavLink
+            as={NavLink}
+            to="/Scrapbook"
+            className={activeLink === "Scrapbook" ? "active" : ""}
+          >
+            Scrapbooking
+          </NavLink>
+          <NavLink
+            as={NavLink}
+            to="/Play"
+            className={activeLink === "Play" ? "active" : ""}
+          >
+            Play
+          </NavLink>
+          <NavLink
+            as={NavLink}
+            to="/About"
+            className={activeLink === "About" ? "active" : ""}
+          >
+            About
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-export default Nav;
+export default NavigationBar;
