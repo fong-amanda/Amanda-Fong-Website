83000c7e5230d48e6f235642e435589d18eefa20
fong-amanda <fong.m.amanda@gmail.com>
2024-03-13 19:30:04 -0400

    work in progress

--- src/Portfolio/Projects/CherryCrisis/cherryCrisis.js
+++ src/Portfolio/Projects/CherryCrisis/cherryCrisis.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Nav from "../../../NavigationBar";
 import './cherry.css';
 import Footer from "../../../Footer/footer";
--- src/Portfolio/Projects/Hippo/hippo.js
+++ src/Portfolio/Projects/Hippo/hippo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Nav from "../../../NavigationBar";
 import './hippo.css';
 import Footer from "../../../Footer/footer";
